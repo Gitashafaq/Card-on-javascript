3125ca57e87b35b0dec038b059405029aeb50068
Gita Shafaq <gitashafaq2613@gmail.com>
2024-04-01 09:13:36 -0700

    Add files via upload

---
+++ card on java script/app.js
@@ -0,0 +1,627 @@
+// let marks=60;
+// if(marks >=90 && marks <=100){
+//     console.log("A Grade");
+// }
+// else if(marks >=80 && marks<=89){
+//     console.log("B grade");
+// }
+// else if(marks >=70 && marks<=79){
+//     console.log("C grade");
+// }
+// else if(marks >=60 && marks <=69){
+//     console.log("D grade");
+// }
+// else if(marks >=55 && marks <=59){
+//     console.log("pass");
+// }
+// else if(marks <=55 && marks >=0){
+//     console.log("faild");
+// }
+// else{
+//     console.log("Invalid Marks");
+// }
+
+
+
+
+
+
+
+
+// ternary operator
+
+// let result;
+// (10 == 10)? result="equal": result="not equal";
+//    console.log(result)
+
+
+
+
+
+// switch case
+
+
+// let day =6;
+
+
+// switch(day){
+//    case 1:console.log("sat");
+//    break;
+//    case 2:console.log("sun");
+//    break;
+//    case 3:console.log("mon");
+//    break;
+//    case 4:console.log("tus");
+//    break;
+//    case 5:console.log("wen");
+//    break;
+//    case 6:console.log("thu");
+//    break;
+//    case 7:console.log("fri");
+//    break;
+//    default:console.log("Invalid day")
+// }
+
+
+
+
+
+
+
+
+// let day=2
+
+// switch(day){
+//   case 1: case 2: case 3: case 4:console.log("sat")
+
+// }
+
+
+
+
+
+
+// let marks =90;
+// switch(true){
+//    case(marks>=90 && marks<=100): console.log("A Grade");
+//    break;
+
+//    case(marks >=70 && marks <=89): console.log("B grsde");
+//    break;
+
+//    case (marks>=60 && marks<=69): console.log("C grade")
+//    break;
+   
+//    default:console.log("Invalid Number")
+// }
+
+
+
+
+
+
+
+
+
+// alert("dont delet this");
+
+// alert(10+10);
+
+
+
+// confirm("are you sure ");
+
+// console.log(confirm("are you sure you want to delete this images?"));
+
+
+
+
+
+
+// let result=confirm("are you sure you want to delete?")
+// if(result){
+//    console.log("image deleted");
+// }else{
+//    console.log("image not deleted");
+// }
+
+
+
+
+
+
+
+
+
+// prompt();
+
+// let result=prompt();
+// console.log(result);
+// document.write(result);
+
+
+// let num1=prompt("Enter anumber");
+// let num2=prompt("entre a number");
+//    let total=Number(num1)+Number(num2);
+//    console.log(total);
+
+
+
+
+
+// let num1=prompt("enter your nember");
+// let op=prompt("enter your operator");
+// let num2=prompt("enter your nember");
+// let total=0;
+// if(op=="+"){
+//    total=Number(num1)+Number(num2);
+// }
+// else if(op=="-"){
+//    total=Number(num1)+Number(num2);
+// }
+// else if(op=="**"){
+//    total=Number(num1)+Number(num2);
+// }
+// console.log(total);
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+// let marks="No";
+// let result=0;
+// if(marks="yes"){
+//     console.log("jsdiosijkiojos");
+//     document.write("kjiosjiksjoisk")
+// }else if(marks="NO"){
+//     console.log("jksiujskjiosko;lsk");
+//     document.write("jowsioiw.pow,qkpq")
+// }
+
+
+
+
+
+
+// function demo(){
+//     console.log("welcome to js;")
+// }
+// demo();
+
+
+
+
+// function totalMarks(name,html,css,js,bootstrap){
+//          let total =html+css+js+bootstrap;
+
+//          console.log(name +"s total marks: " +total);
+//          document.write(name +"s total marks:"+total+"<hr>")
+// }
+// totalMarks('Ahmad',88,90,78,88);
+// totalMarks('karim',100,100,100,100);
+// totalMarks('mohammad jan',74,87,39,29);
+
+
+
+
+
+
+// time 
+
+// let now = new Date();
+
+
+// let hours= now.getHours();
+// let minutes= now.getMinutes();
+// let seconds= now.getSeconds();
+// if(hours>12){
+//     let merdim="pm";
+// }else{
+//     let merdim="Am";
+// }
+// let properTime = hours +":"+ minutes +":" + seconds;
+// console.log(properTime);
+
+
+// month year day
+
+
+// let now=new Date();
+// let year=now.getFullYear();
+// let month=now.getMonth();
+// let day=now.getDay();
+
+
+
+
+// let months =['J','F','M','A'];
+// let weakDays=['S','M','T','W'];
+
+// let properDate = year +"/"+ months[month] +"/"+ weakDays[day];
+// console.log(properDate);
+
+
+
+
+
+
+
+
+
+// month year date
+
+// let now=new Date();
+// let year=now.getFullYear();
+// let month=now.getMonth();
+// let date=now.getDate();
+
+
+// let properDate= year +"/"+ month +"/"+ date;
+// console.log(properDate);
+
+
+
+
+
+
+// let now = new Date();
+// console.log(now);
+
+
+
+// let now =new Date();
+// console.log(now.toLocaleString());
+
+
+
+
+
+
+// let now=new Date();
+// let year=now.getFullYear();
+// console.log(year);
+
+
+
+
+// let now =new Date();
+// let date=now.getDate();
+// console.log(date);
+
+
+
+// let now =new Date();
+// let month=now.getMonth();
+// console.log(month);
+
+
+
+// let now=new Date();
+// let day=now.getDay();
+// console.log(day);
+
+
+
+// let now=new Date();
+// let year=now.getFullYear();
+// let date=now.getDate();
+// let month=now.getMonth();
+// let properDate=year+"/"+month+"/"+date;
+// console.log(properDate);
+
+
+
+
+
+// math methods
+
+// let num=-100.0;
+// console.log(Math.PI);
+
+
+
+// let num= -100;
+// console.log(Math.abs(num));
+
+
+
+// let num=100;
+// console.log(Math.ceil(Math.random()*1000000000));
+
+
+
+
+
+
+
+
+// let num=100;
+// console.log(Math.random()*10);
+
+
+
+// let num=100;
+// console.log(Math.pow(4,4));
+
+
+
+
+
+// let num=100.0;
+// console.log(Math.sqrt(12));
+
+
+
+
+
+// let num=100;
+// console.log(Math.min(100,5,200,1000));
+
+
+
+
+
+// let num=100;
+// console.log(Math.max(12,354,455,4664));
+
+
+
+
+// let num=  100.8888;
+// console.log(Math.trunc(num));
+
+
+
+// let num =100.3;
+// console.log(Math.round(num));
+
+
+
+// let num =100.6;
+// console.log(Math.floor(num));
+
+
+
+// let num=100.10;
+// console.log(Math.ceil(num));
+
+
+
+
+// let num=100.44;
+// console.log(num.toFixed());
+
+
+
+// let num=100;
+// console.log(Number.isInteger(num));
+
+
+
+// let num=0.5;
+// console.log(Number.isFinite(num));
+
+
+// let num =100.5;
+// console.log(Number.parseFloat(num));
+
+
+
+// let num =100.55;
+// console.log(Number.parseInt(num));
+
+
+
+// let num ="100";
+// console.log(Number(num)+200);
+
+
+
+
+
+// let a=100;
+// console.log(a.toString()+100);
+
+
+
+// let string="Lets learn java script";
+// console.log(string.slice(5,8));
+
+
+// let straing="Lets learm java script";
+// console.log(straing.repeat(200));
+
+
+
+
+// let see="let learn java script";
+// console.log(see.split(" "));
+
+
+
+
+
+
+
+// let string="lets learn java script";
+// let string1="lets learn java";
+// console.log(string.concat(string1));
+
+
+
+
+
+// let straing="lets learn java script";
+// console.log(straing.charAt(1));
+
+
+
+
+
+
+// let straing="  learn javascript    ";
+//   console.log(straing.trim());
+
+
+
+
+// let string="java script is the bets java script";
+// console.log(string.replace(/java script/g,'css'));
+
+
+
+
+
+
+
+
+
+// let string="java script is the bets java script";
+// console.log(string.replace('java script','css'));
+
+
+
+
+
+
+// let string="java script is the best languages";
+// console.log(string.match(/e/g));
+
+
+
+
+
+
+
+// let straing="java scrpt is the best languages";
+// console.log(straing.indexOf("n"));
+
+
+
+
+
+
+// let string="dghuueikso,ke,idmiudkuwklsiskj";
+// console.log(string.search("e"));
+
+
+
+
+// let straing="iwooooiiiiiiiiiiiiiiiiiiiiiiiiii";
+// console.log(straing.endsWith("i"));
+
+
+// let straing="kkkkkkkkkkkkkkkkkkkkkkkkkkkkkk";
+// console.log(straing.startsWith("k"));
+
+
+
+// let straing="oooooooooooooooooooooooooo";
+// console.log(straing.includes('trtr'));
+
+
+// let straing="AAAAAAAAAAAAAAAAAAAAAA";
+// console.log(straing.toLowerCase());
+
+
+// let straing="Java ksssssssssssssssss";
+// console.log(straing.length);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
